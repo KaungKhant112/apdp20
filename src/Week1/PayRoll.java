8fde7fe13bdabb40dbe9989c4a2beb31d5a3e2d6
KaungKhant112 <msms78128@gmail.com>
2024-01-09 15:15:57 +0630

    payroll is defined

--- src/Week1/PayRoll.java
+++ src/Week1/PayRoll.java
@@ -1,5 +1,24 @@
 package Week1;
 
 public class PayRoll {
+    float payRate; //camel case
+    float workHour;
+    PayRoll(float pr,float wh)// 2 arguments constructure
+    {
+        payRate = pr;
+        workHour = wh;
 
+    }
+
+    public void displayGrossPayandNetPay()
+    {
+        float grossPay=payRate*workHour;
+        float netPay=grossPay- (grossPay*0.15f);
+        System.out.printf("  Gross Pay is %20.2f \n net pay is %20.2f",grossPay, netPay);
+    }
+
+    public static void main(String[] args) {
+        PayRoll payRoll=new PayRoll(20,8); //create PayRoll object
+        payRoll.displayGrossPayandNetPay();
+    }
 }
