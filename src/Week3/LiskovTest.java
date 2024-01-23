bbf2112607fd19e8b611d0ef0c155db50dd0147b
KaungKhant112 <msms78128@gmail.com>
2024-01-23 15:43:47 +0630

    box classes with void method, return method and parameter method are added

--- src/Week3/LiskovTest.java
+++ src/Week3/LiskovTest.java
@@ -2,11 +2,21 @@ package Week3;
 
 public class LiskovTest {
     public static void main(String[] args) {
-        Rectangle r1=new Rectangle(20,10);
-        Square sq1=new Square(20);
-        Rectangle r2=new Rectangle(15,30);
-        Square sq2=new Square(30);
+        Rectangle r1 = new Rectangle(20, 10);
+        Square sq1 = new Square(20);
+        Rectangle r2 = new Rectangle(15, 30);
+        Square sq2 = new Square(30);
         //super type variable can store sub type objects
-        Quadrangle[] quadrangles= {r1,sq1,r2,sq2};
+        Quadrangle[] quadrangles = {r1, sq1, r2, sq2};
+        //enhanced for loop
+        for (Quadrangle quadrangle : quadrangles) {
+            float area = quadrangle.area();
+            System.out.println("area is " + area);
+        }
+        //traditional for loop
+        for (int i = 0; i < quadrangles.length; i++) {
+            float area = quadrangles[i].area();
+            System.out.println("area is " + area);
+        }
     }
 }
